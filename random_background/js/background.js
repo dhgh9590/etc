363e1b35015aa6179491bfadd2ccd84b0b1ebd52
hoseol dh <dhgh9590@naver.com>
2021-11-27 11:32:32 +0900

    eleventh commit

---
+++ random_background/js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpg","1.jpg","2.jpg","3.jpg"] //img폴더 안에 있는 이미지 파일 이름과 같아야 함
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");//img태그 만들기
+
+bgImage.src = `img/${chosenImage}`; //루트 img폴더
+let section = document.querySelector("#section");
+
+section.appendChild(bgImage);
